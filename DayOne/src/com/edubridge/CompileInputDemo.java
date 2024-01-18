b88f6b923879932cb7bfac8e545ed4458ead978d
LENOVO <LENOVO@LENOVO-PC>
2024-01-18 13:34:34 +0530

    commit

---
+++ DayOne/src/com/edubridge/CompileInputDemo.java
@@ -0,0 +1,28 @@
+package com.edubridge;
+
+public class CompileInputDemo {
+
+	public static void main(String[] args) {
+		
+        byte num1=127;
+        short num2=5896;
+        int num3=69;
+        long num4=852364;
+        String str="Harshal";
+        char var='h';
+        float num5=85.5f;
+        double num6=58.120d;
+       
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(var);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(str);
+        
+        
+	}
+
+}
